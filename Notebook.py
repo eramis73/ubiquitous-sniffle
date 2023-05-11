8d9dea745d847919346d12e7b08bed5e70b315c5
eramis73 <130156545+eramis73@users.noreply.github.com>
2023-05-11 16:02:21 +0300

    DataScienceEcosystem

---
+++ Notebook.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 
+# 2. R
+# 
+# 3. SQL
+# 
+# 4. Julia
+
+# ## Objectives
+
+# -Data Exploration and Analysis
+
+# -Machine Learning, Modelling
+
+# ## List Popular Languages For Data Science
+
+# -Python
+# -R
+# -SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+#     
+# 1. Numpy
+# 
+# 2. Pandas
+# 
+# 3. scikit-learn
+# 
+# 4. Tensor-Flow
+# 
+# 5. Pytorch
+# 
+
+# In[ ]:
+
+
+
+
+
+# Data Science Tools
+
+#  Jupiter Notebook
+# 
+
+#  R Studio
+
+#  Apache Zeppelin
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# 3+5
+
+# 4+9+8
+
+# 19*58
+
+# In[ ]:
+
+
+##This a simple arithmetic expression to mutiply then add integers
+
+
+# In[12]:
+
+
+(3*4)+5
+
+
+# In[13]:
+
+
+hours=minutes/60
+minutes=200
+hours
+
+
+# ## Author
+
+# Enes Ramis GUL
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# 
